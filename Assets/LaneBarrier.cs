64a11d715f4f84ddcf7d5f157aec43b9932e489b
Archit Vishnoi <archit969@gmail.com>
2021-12-16 09:04:17 -0500

    Final Commit

---
+++ Assets/LaneBarrier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneBarrier : MonoBehaviour
+{
+	private Manager manager;
+	private void Start()
+	{
+		manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other.name);
+		if (other.gameObject.GetComponent<BattleUnit>().type == "Combat" || other.gameObject.GetComponent<BattleUnit>().type == "Siege")
+		{
+			manager.UpdateKilled(other.gameObject.tag, other.gameObject.GetComponent<BattleUnit>().type);
+			Destroy(other.gameObject);
+		}
+	}
+}
